d2089cc61365549edbb3e7268c682792220d7ef8
Isaac <iico7no2@gmail.com>
2024-10-02 21:44:30 -0500

    Update

--- scripts/workflows/pull-web-artifacts/pull-web-artifacts.js
+++ scripts/workflows/pull-web-artifacts/pull-web-artifacts.js
@@ -1,5 +1,6 @@
-const fs = require('node:fs');
+const fs = require('fs-extra');
 const core = require('@actions/core');
+const { match } = require('path-to-regexp');
 
 // const allowedWebBranches = input('allowedWebBranches', '');
 // const artifactInfo = input('artifactInfo', '');
@@ -33,16 +34,39 @@ function input(name, def) {
 }
 
 function main() {
-  // print files in checked out config_branch branch.
-  console.log('reading dir `.`');
-  fs.readdirSync('.').forEach((file) => {
-    console.log(file);
-  });
+  const branchConfig = fs.readJsonSync('./config_branch/config_branch.json');
 
-  console.log('reading dir `config_branch`');
-  fs.readdirSync('./config_branch').forEach((file) => {
-    console.log(file);
-  });
+  const artifactUrl = parsedArtifactInfo['web-zip-url']['ubuntu-latest'];
+
+  const fn = match('/:owner/:repo/actions/runs/:run_id/artifacts/:artifact_id');
+
+  try {
+    const url = new URL(artifactUrl);
+
+    if (url.origin !== 'https://github.com') {
+      throw new Error(
+        `Origin must be https://github.com, but was '${url.origin}'.`
+      );
+    }
+
+    const obj = fn(url.pathname);
+    console.log('obj');
+    console.log(obj);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+
+  // // print files in checked out config_branch branch.
+  // console.log('reading dir `.`');
+  // fs.readdirSync('.').forEach((file) => {
+  //   console.log(file);
+  // });
+
+  // console.log('reading dir `config_branch`');
+  // fs.readdirSync('./config_branch').forEach((file) => {
+  //   console.log(file);
+  // });
 }
 
 main();
