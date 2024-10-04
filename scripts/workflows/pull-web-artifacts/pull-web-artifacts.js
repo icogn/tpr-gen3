3a4d2bbd6c0aad5121745c8feba28f1f9810a29f
Isaac <iico7no2@gmail.com>
2024-10-04 12:38:23 -0500

    Update

--- scripts/workflows/pull-web-artifacts/pull-web-artifacts.js
+++ scripts/workflows/pull-web-artifacts/pull-web-artifacts.js
@@ -4,36 +4,37 @@ const core = require('@actions/core');
 const { match } = require('path-to-regexp');
 
 // const allowedWebBranches = input('allowedWebBranches', '');
-const artifactInfo = input('artifactInfo', '');
-
 // console.log(`allowedWebBranches:${allowedWebBranches}`);
 
-const parsedArtifactInfoIn = JSON.parse(artifactInfo);
-console.log('parsedArtifactInfoIn');
-console.log(parsedArtifactInfoIn);
+// const artifactInfo = input('artifactInfo', '');
+// const parsedArtifactInfoIn = JSON.parse(artifactInfo);
+// console.log('parsedArtifactInfoIn');
+// console.log(parsedArtifactInfoIn);
+// const parsedArtifactInfo = parsedArtifactInfoIn;
 
-const parsedArtifactInfo = parsedArtifactInfoIn;
-
-// const parsedArtifactInfo = {
-//   byTriple: {
-//     'x86_64-pc-windows-msvc': {
-//       name: 'web-windows-latest',
-//       'web-zip-sig':
-//         'WgXK0m70/HJtb8jVfzDRhNLnGAnhRjAR5GytY2XRLixm82dddijpm6vZnJXE2J+wnNQG2a4PCuczPE2jgLeS2c1KsyYh81cFv+OhFMP3SLXR1OFHqhQ7z0Cy2D/cK50lSKUPNSLisD4HIbGDxRqf9oJ97DoEOvEOpDtuTnwOH0K/pj2WdjzCiqxd8RnljHS8+K+eMdY/yqSWEQDeQO/TaEprIauEeS2oKqcYcetzhkW8KinQofjtFWnlZ01g1RUPM5kra2yzs0pGsxrtzvHRyUWFm808zRQQEUqsNgLzYRbfQYt2Q47g/XDZfdjzrH+f1lmud7SQPOzWkEmikf+RYcVx9jab1lAT5ap4GtAf0yF7zDJHhzK3udgnM2sqcJFcMZ9Oc6DVSXfo/4aXmSuGTEegI8N45YgJtkamV/FqiSCxGvYuzxCZLrOH+qr1YmN69kmVFEI4WvfuDg46OPc9j8j/7JLASKUd3VXEwnitqqyA3G+gPz/Irdxy5lygiYuxOjdSOJDChcpLI4P1YltPKEVAtw8Yo6eQtg0S4OYijlZfjzulsV+mOiL8zw4CvsuxFuEoOcnVZTvbbESZZxCSYVYuix3S/ijmV08lf+/utA1FfxIbbA2QZnSZuOtbQq2tcF6g67726vlFdVrpuHUBOylRjoB2XaibY+j9Nbc56pU=',
-//       'web-zip-url':
-//         'https://github.com/icogn/tpr-gen3/actions/runs/11171484607/artifacts/2013581135',
-//     },
-//     'x86_64-unknown-linux-gnu': {
-//       name: 'web-ubuntu-latest',
-//       'web-zip-sig':
-//         'iFxLPOvyx17Sot1+3ySPHF2EFVrYS4agJckbFgp/NsM5epwccloVyUs3V8/l1tZ7/pBSq40J//1bF0VFMSHnAMkV5UoCwLcLTbLaNPjqwLOyU7J/QoKUIRuVShVYADtwO8YEJW+qWxOyk+N02PdAEMtv2G2DtzIiH+G3hg4hoAWs8iquoV8L2rDGARMNAvV6tmHeFqZI7MVDriVTpaqU7etLVtz9Cla1RFlKcB0iil8UhOEdcgwUndjfqEDztT4j9Qyk+6JxG1V5HQ/XLWUy/bKjhiDx+gkT6VC4Lz71jcA+TQUW4Czdpl2wr2Qeex2ZyuOTDzNf7yUN2Sq0ftIHdfvOD/8Wo9VGtOm9pZehQbY6lraMFCTCBlT/bcR2KvCzn0ryj+adg8Cvl05xvmfY8j7Uf+uZyNssudILemAyPC3o+J0iL74DxtEFFkfKiNlUD8QEd+ZSuXorH0C4lXdX7brbnmPDL9SD7BuHGmAeE5isORjGLRi1zHpv0Nvxi0My5vid/s2iWk0ChLN0+dr4BgfrTN944izn+Hq3yPcchfzll7KLdGHpPOB1hQ70yow5ZR8o2d+HLhSREYWELukTcsfKcE+e/qM3516eVEy5TkyBdWSmPIxbTExja92Z5JCuVTU932fM5jaFAFtU6m/3fb/b/YAeyf4POz1o4+IhisE=',
-//       'web-zip-url':
-//         'https://github.com/icogn/tpr-gen3/actions/runs/11171484607/artifacts/2013579531',
-//     },
-//   },
-//   signature: 'exampleSig',
-//   timestamp: '2024-10-03T23:55:46.302Z',
-// };
+const parsedArtifactInfo = {
+  byTriple: {
+    'x86_64-pc-windows-msvc': {
+      name: 'web-windows-latest',
+      'web-zip-sig':
+        'L7ASVXwGPDayCT6QdL2nZPSSfF0VmXl/GIn6vv/21o22e9oOM5OP27yhAYbu0+g6UriwbqhL4uBBoF40mAF926E54A1upa1y/sbe1+p51MVQbaArMa06HDVr8U7TPSQwxfNXKNr6DLO6Wfc7XFNVOcPSYeYbrsY1jxD3rTFguk6q9PcHtB1Tyshb/yj9T9e0g5QOt1+71qhLJDilRIy9U0IclOauqGH4O5qJUpqaZ2LxnzfkPwapwd8N6EINcOSReEjhcMjcujApqQWMOMhRiTfQQqM3z7PC9SE1/CGP+Mfa1acRVZKxJZ2SwPR8RSXFXuRAhmjowPWfgYWGBHo8YTRhGHhr+LmzoHikoeSjjMrjKuKkWpGejXUVf8lBaeUxa3JSdtHn6N2GsGd8Cxwxv58nbYXcJVopQ7acGCEij8pg6pif42bQZPUCUlO5w526ySmD40tPUV77iG449Phho2Dn3uyM73sI+aR/lH+uZhRCv1K6Ud47dpRrabdv27+QBSK8tGJUs2cyjUh0ppr+lWWZBbDW5zgmEKLHM/vnREcqaDCeF76FwLvKRYWRu9QaQTJk2w4WRpvso0TEUv3+xOfQVK98+p1qvKhFV2W9v/SzcEuocrAgejs56YeipUircGZ+f9ZG125+k5fno5gosmLEygp7F14+kXllf0fd+wo=',
+      'web-zip-url':
+        'https://github.com/icogn/tpr-gen3/actions/runs/11184341001/artifacts/2016988768',
+    },
+    'x86_64-unknown-linux-gnu': {
+      name: 'web-ubuntu-latest',
+      'web-zip-sig':
+        'fwmGD2USd7dhL4j44GTK8Ofd+/NQDWEZy8xp6KehVkSinmtDlfkidJYsl/BpcydFtatSoRnyeJwi8DoWN0o3i4kssZQ1C5f2CXGiI1UmGNrPZZ84Qkr+TP/O99KF2GDkUiQfd4m8Kq87HJWxU9CeJMAR5zljnndOFrLIVMYl2wdX5MtADfGtFiTsaf9yW1TSOy0JZdNrqpgVwkCcnC0/P9p8i4fV/UNYDrRKNfZfL9D4Y2DUiW+7aGeYDN2pAW3kCiIt8pj8+X9NmlbBvJJjswmegSarmW4uzw1R75zXruuXpYyNEYnP94pV0Sx4Uxel6xpfdbhA9FRYAIAljphpmSKCPHGWpPLs2yRvPXz6JJkyhws7y1eyqa99r6M+QjP0Ari7VTmXaKycIxTpkK2cVATanca0uNLEiLyVpiyX4mwTvJlwqY7nCavDRrZ3NtA1YcZGzmkMMhaUh0hMjVerfz05Ykj5Mvc/uvKEI9eOQV1wxtRTa44Kb4NMl/b0hOGr1kLM3ypGBvU+Or2rmty5A+WRKJ7tkL6Dk2QMHrkoyOGQ6hmThtlhCPqoMu0xlKR/ro0hkDGqbRKnQhb0GGX6hvEShrJOlvleXGkj8CS/cQdV8lAq91VX3hBC5HPe/tG12FegZwg94/8PR9XtS3s+7rpAQtIgFCNIC+FSgqd5kPo=',
+      'web-zip-url':
+        'https://github.com/icogn/tpr-gen3/actions/runs/11184341001/artifacts/2016985763',
+    },
+  },
+  signature:
+    'hIacfZl23Ti42x2le0zHwvJKzJFbTcE4lYco4za9KOWjAt5ziuYPcv4R1INGR3HNUrqWpl8JFBWA8Y5NvrMaBxoQTNLOQspF+3unuD6OFEoZwGCNG9mbleqRkevi7o1P7GmNqmhgityfEUV+xsuabWWQBVcXcZQLHxz4cgzlXhvAVCJLfIuM38r5zExoDej+RrTuyffQPMvRMd4OEPtLuPkKjW7EySVBPR8Jl3xH2/dOjCypL5Wvb06n8+lnFoNgVEB1ZlZQJ97vOBvBKU1PC/YjQ58BUgvhHLaANHZEa5SummNcqKCM2/ZAzSxHj5aQhvgEfk+QfYRHC1S9QNlucGVycQWrATjjrvumey46vOfPLx3/qfCaZLuoCH3zs2jEWQ8V0ptdhBF55FNWZIdisf2vr/qsarkLlJj9P3Mnt9ttuf/VZvW4F9B2+6Rda0M7JN4NYTybzRb7x5dkV6l0cth1OEjjCtjKIYg7St+RDv85VN/tG6LAiAmJq1HqsDS3zclXuVnNpHxvLCmtcmfrphWLcJK+W69dQmUbcbcqIfKlA5Zgv/iL2MSLWBoYr1ocJKLrwlAp9EFtk9UQRyeGrsJzwYNAojMLy0tgC6Phv1eheX0PZdnhf9IbLhDWfUCqxp6FOdQR98ck7c73pdhvyIVh9agMLfYY/1nXzVuwGac=',
+  timestamp: '2024-10-04T17:33:52.754Z',
+};
+const signature = parsedArtifactInfo.signature;
+parsedArtifactInfo.signature = undefined;
 
 function input(name, def) {
   let inp = core.getInput(name).trim();
@@ -56,6 +57,30 @@ async function run() {
     //   required: false,
     // }),
   };
+
+  // start verify info signature
+  // This is a hardcoded test value
+  const publicKey =
+    '-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA3BIwGEBi9flZfX6W5y09\nM0S9kV8mXZSL1mkOVx/B18v7kBCRsquCzSs5ot7DChJcyYqanuzWyM14HK7gnLBp\nWEU5PQOhag+WW8pkWgHjlTauB+sEd9X7MPPU5o5OR61nCzYIToNGxLx5NXksj5A4\nuUkS4eKaZ336aZBAj/dvOEPQA1m3azwIBbmxdDadDki76Ykjz35yUgtZyF/x8Bpt\n7YRY0kBwHdq57EVBaMQl0uSfCaFGPx7ez36OkWvhUyfCUy5ApyPoeDK36gIcOuMr\nS6CyLEh+Y0JmZSAzLgSPnh1N7S7F4Lf+IKoiws5Be6xvot16nSRpZc5NJAfyu/MU\nfmy5kcB5TqcQcWh61d4s4p8a1FnU9M0prTOVOHWtkG08tmlniHQXX8igrnRgvcIo\nHbMCVcIrOSrwsSeyabtxXfDpwp2+orr6RNJQKOlc8iCCf8y6CYyFlmftO0WN/+gc\ndc3hIRwmlefg/wmTyS68SvXLA1AvM9tlQ4n0oiYpL6MO5c2828jg3Ytr76FAqHtp\nfrXwRHqAAqq5yvQjuWt5r942ozIBbsElq0cHyguchMw2MXz9m6+rBnuJy8SL1M47\ndgy287Skw4QWKq6G4LnIZp9Na0+svZSiPVD/fQ1sDFOHifUJITNNXXyDdRFd+8DT\nTMiwi2Fsd1kDmGS0eP/TcX0CAwEAAQ==\n-----END PUBLIC KEY-----\n';
+
+  const dataToVerify = JSON.stringify(parsedArtifactInfo);
+  console.log('dataToVerify:');
+  console.log(dataToVerify);
+
+  const aaa = await new Promise((resolve, reject) => {
+    verify('RSA-SHA256', dataToVerify, publicKey, signature, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+
+  console.log('aaa:');
+  console.log(aaa);
+
+  // end verify info signature
 
   const branchConfig = fs.readJsonSync('./config_branch/config_branch.json');
 
