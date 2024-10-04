30ce9b8ccb82e6a8a96497be3297ff6d5fb5bb41
Isaac <iico7no2@gmail.com>
2024-10-04 12:11:01 -0500

    Update

--- scripts/workflows/trigger-pull-artifacts/trigger-pull-artifacts.js
+++ scripts/workflows/trigger-pull-artifacts/trigger-pull-artifacts.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { createSign } = require('node:crypto');
 
 const token = core.getInput('token');
 const octokit = github.getOctokit(token, { required: true });
