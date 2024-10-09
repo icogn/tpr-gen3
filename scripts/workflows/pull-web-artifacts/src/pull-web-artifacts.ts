5d884dc94f352879e777d2350198d5bcf12ba2ef
Isaac <iico7no2@gmail.com>
2024-10-09 18:33:19 -0500

    Adjust

--- scripts/workflows/pull-web-artifacts/src/pull-web-artifacts.ts
+++ scripts/workflows/pull-web-artifacts/src/pull-web-artifacts.ts
@@ -86,6 +86,7 @@ const configIn = fs.readJsonSync(CONFIG_FILEPATH);
 // const { success, error } = configSchema.safeParse(configIn);
 const { error, data } = configSchema.safeParse(configIn);
 if (error) {
+  console.log('config zod error:');
   core.setFailed(error.message);
   process.exit(1);
 }
