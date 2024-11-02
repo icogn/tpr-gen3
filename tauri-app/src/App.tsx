050e2197649283414205260b28621a223a3e7583
Isaac <iico7no2@gmail.com>
2024-11-01 23:17:49 -0500

    Show iframe in UI

--- tauri-app/src/App.tsx
+++ tauri-app/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import { invoke } from '@tauri-apps/api/core';
 import './App.css';
 // import { Command } from '@tauri-apps/plugin-shell';
@@ -64,23 +63,8 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
+    // <div className="container">
+    <div className="container2">
       <form
         className="row"
         onSubmit={(e) => {
@@ -97,6 +81,8 @@ function App() {
       </form>
 
       <p>{greetMsg}</p>
+
+      <iframe src="http://127.0.0.1:3000" className="testing"></iframe>
     </div>
   );
 }
