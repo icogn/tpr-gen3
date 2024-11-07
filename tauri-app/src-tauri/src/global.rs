9e238546bc855d192bf5400b8a0551dcca50f161
Isaac <iico7no2@gmail.com>
2024-11-06 22:24:23 -0600

    Add user_repo to VARS

--- tauri-app/src-tauri/src/global.rs
+++ tauri-app/src-tauri/src/global.rs
@@ -6,21 +6,27 @@ use serde_json::Error;
 static BUILD_VARS_JSON: &str = std::include_str!("./build_vars.json");
 
 pub struct Vars {
+    pub user_repo: &'static str,
     pub central_name: &'static str,
 }
 
 #[derive(Deserialize)]
 struct BuildVarsJson<'a> {
+    user_repo: Option<&'a str>,
     central_name: Option<&'a str>,
 }
 
 pub static VARS: LazyLock<Vars> = LazyLock::new(|| {
     let mut out = Vars {
+        user_repo: "icogn/tpr-gen3",
         central_name: "stable",
     };
 
     let res: Result<BuildVarsJson, Error> = serde_json::from_str(BUILD_VARS_JSON);
     if let Ok(root) = res {
+        if let Some(x) = root.user_repo {
+            out.user_repo = x;
+        }
         if let Some(x) = root.central_name {
             out.central_name = x;
         }
--- tauri-app/src-tauri/src/query.rs
+++ tauri-app/src-tauri/src/query.rs
@@ -1,6 +1,7 @@
 use deduplicate::Deduplicate;
 use std::time::Duration;
 // use deduplicate::DeduplicateError;
+use crate::global::VARS;
 use deduplicate::DeduplicateFuture;
 use std::sync::Arc;
 
@@ -53,23 +54,37 @@ pub fn get(key: usize) -> DeduplicateFuture<String> {
         // TODO: this should be be provided as a build resource or something
         // similar for production builds? Also need to know which "central" to
         // use.
-        let first = reqwest::get("https://raw.githubusercontent.com/icogn/tpr-gen3/refs/heads/config_branch/config_branch.json").await;
 
-        let text = match first {
+        let endpoint = format!(
+            "https://raw.githubusercontent.com/{}/refs/heads/config_branch/config_branch.json",
+            VARS.user_repo
+        );
+
+        let req_result = reqwest::get(endpoint).await;
+        let resp = match req_result {
+            Ok(x) => x,
+            Err(_) => {
+                return None;
+            }
+        };
+        let resp_text = resp.text().await;
+        let body = match resp_text {
             Ok(x) => x,
             Err(_) => {
                 return None;
             }
         };
 
-        let aa = text.text().await;
+        // Need to figure out what we are returning from this.
 
-        let body = match aa {
-            Ok(x) => x,
-            Err(_) => {
-                return None;
-            }
-        };
+        // The frontend needs to know the following:
+        // - normalized data about each branch (includes the latest version info
+        //   if we successfully get this info)
+        // --- We should also only include this info if there is a siteZip that
+        // matches the current platform.
+        // - Ordered list of the branch_names so we know the order for the select
+        // - It does not need to know the detailed asset_info.json info.
+        // --- However, we do need to store this data on the backend.
 
         // TODO: this call seems to take about 320 ms the first time, and 43 ms
         // after that, so we should cache the result on the rust side for some
@@ -80,11 +95,30 @@ pub fn get(key: usize) -> DeduplicateFuture<String> {
 
         tokio::time::sleep(Duration::from_millis(3000)).await;
 
-        // .text()
-        // .await?;
+        // We must be able to return to the frontend info about the available
+        // branches. We also must know the branch to default to /
+        // current_branch. This info would be stored in the core DB to use on
+        // startup. When the frontend requests this info, it happens async. We
+        // merge the static info with the info we get dynamically from the
+        // calls. Some of this info might not be there if the call fails or the
+        // user is offline, etc. For example, the latest version for a site
+        // might not be known, so in that case, the frontend will not know that
+        // an update is available for that site, which is fine.
 
-        // .text()
-        // .await?;
+        // When the user successfully "installs" a site, we MUST store the
+        // relevant info about that site in the core DB (such as its name). We
+        // probably don't actually need to store any other info like
+        // owner/repo/publicKey since this info is not relevant when offline.
+
+        // Maybe the frontend should handle asking about the startup data and
+        // the fetch data separately as well? We want to have the startup data
+        // ASAP, and we can refresh the list with a slight delay.
+
+        // The startup data would just be the installed branch data.
+
+        // TODO: next thing is probably getting the core DB working so we can
+        // read/write data about the installed branches and the default/current
+        // branch.
 
         Some(format!("body = {body:?}"))
     };
