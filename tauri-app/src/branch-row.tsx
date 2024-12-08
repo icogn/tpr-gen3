f8b5b76373ad7ead4f4797d982af42e15157de56
Isaac <iico7no2@gmail.com>
2024-12-07 20:03:05 -0600

    Move row dialog

--- tauri-app/src/branch-row.tsx
+++ tauri-app/src/branch-row.tsx
@@ -1,13 +1,51 @@
+import { MouseEventHandler, useState } from 'react';
 import { ResolvedBranch } from './types';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 
 type BranchRowProps = {
   branch: ResolvedBranch;
 };
 
 export function BranchRow({ branch }: BranchRowProps) {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="border px-4 py-2 mb-2 rounded cursor-pointer hover:bg-gray-700">
-      {branch.displayName}
-    </div>
+    <>
+      <div
+        className="border px-4 py-2 mb-2 rounded cursor-pointer hover:bg-gray-700"
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
+        {branch.displayName}
+      </div>
+
+      <Dialog
+        open={open}
+        onOpenChange={(val) => {
+          console.log(`val: ${val}`);
+          setOpen(val);
+        }}
+      >
+        {/* <DialogTrigger>Open</DialogTrigger> */}
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>{`${branch.displayName}`}</DialogDescription>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
--- tauri-app/src/settings-modal.tsx
+++ tauri-app/src/settings-modal.tsx
@@ -1,15 +1,9 @@
+import { useState } from 'react';
 import { BranchRow } from './branch-row';
 import styles from './settings-modal.module.css';
 import { useBranchesQuery } from './state/api-slice';
 import { ResolvedBranch } from './types';
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog';
+import { X } from 'lucide-react';
 
 type SettingsModalProps = {
   onClose(): void;
@@ -20,10 +14,13 @@ export default function SettingsModal({ onClose }: SettingsModalProps) {
 
   return (
     <div className={styles.root}>
-      <div className="flex px-4 pb-4 ">
-        <div role="button" className="pt-5 ml-auto" onClick={onClose}>
-          X
-        </div>
+      <div className="flex px-4 py-4 justify-end">
+        <button
+          className="rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
+          onClick={onClose}
+        >
+          <X className="h-4 w-4" />
+        </button>
       </div>
       <div className="flex justify-center">
         <div style={{ width: '100%', maxWidth: 300 }}>
@@ -32,19 +29,6 @@ export default function SettingsModal({ onClose }: SettingsModalProps) {
           })}
         </div>
       </div>
-
-      <Dialog>
-        <DialogTrigger>Open</DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
     </div>
   );
 }
